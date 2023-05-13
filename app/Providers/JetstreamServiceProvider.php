f6a106de75e36a542991ca27de0e92666f20355e
RickySBahaso <ricky@sali-evo.com>
2023-05-13 10:10:43 +0700

    update #1

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\Blade;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class JetstreamServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerComponent('buttonblock');
     }
 
     /**
@@ -45,5 +46,10 @@ class JetstreamServiceProvider extends ServiceProvider
             'update',
             'delete',
         ]);
+    }
+
+    protected function registerComponent(string $component)
+    {
+        Blade::component('jetstream::components.'.$component, 'jet-'.$component);
     }
 }
--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -15,7 +15,7 @@
     </head>
     <body class="font-inter antialiased bg-slate-100 text-slate-600">
 
-        <main class="bg-white">
+        <main class="bg-gradient-to-r from-indigo-500 to-blue-500 " data-primary="indigo-500"  data-tails-scripts="//unpkg.com/alpinejs"        ">
 
             <div class="relative flex">
 
@@ -28,8 +28,8 @@
                         <div class="flex-1">
                             <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
                                 <!-- Logo -->
-                                <a class="block" href="{{ route('dashboard') }}">
-                                    <svg width="32" height="32" viewBox="0 0 32 32">
+                                <a class="flex" href="{{ route('dashboard') }}">
+                                    {{-- <svg width="32" height="32" viewBox="0 0 32 32">
                                         <defs>
                                             <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
                                                 <stop stop-color="#A5B4FC" stop-opacity="0" offset="0%" />
@@ -44,7 +44,11 @@
                                         <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
                                         <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
                                         <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-                                    </svg>
+                                    </svg> --}}
+                                    {{-- <a href="#_" class="flex items-center py-4 pl-6 pr-4 md:pl-4 md:py-0"> --}}
+                                        <img src="{{ asset('images/bgt-logo-white.png') }}" class="w-10 h-10">
+                                        <span class="ml-2 mt-2 text-lg font-black leading-none select-none text-white">Bina Group</span>
+                                      {{-- </a> --}}
                                 </a>
                             </div>
                         </div>
@@ -60,7 +64,7 @@
                 <!-- Image -->
                 <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
                     <img class="object-cover object-center w-full h-full" src="{{ asset('images/auth-image.jpg') }}" width="760" height="1024" alt="Authentication image" />
-                    <img class="absolute top-1/4 left-0 -translate-x-1/2 ml-8 hidden lg:block" src="{{ asset('images/auth-decoration.png') }}" width="218" height="224" alt="Authentication decoration" />
+                    {{-- <img class="absolute top-1/4 left-0 -translate-x-1/2 ml-8 hidden lg:block" src="{{ asset('images/auth-decoration.png') }}" width="218" height="224" alt="Authentication decoration" /> --}}
                 </div>
 
             </div>
